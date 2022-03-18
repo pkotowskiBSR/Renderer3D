3a0ab12a0af5b451f5b7a939e5a1ad28f93e40a4
pkotowskiBSR <pawel.kotowski@bednarska.edu.pl>
2022-03-18 09:22:43 +0100

    2022-03-18

--- Renderer3D/Form1.cs
+++ Renderer3D/Form1.cs
@@ -1,11 +1,15 @@
 using System.Numerics;
 
+
 namespace Renderer3D
 {
     public partial class Form1 : Form
     {
         Pyramid pyramid = new Pyramid(100, 100, 200);
+        Sphere sphere = new Sphere(100, 20, 20);
+
         Camera camera = new Camera(new Vector3(0, 300, 100), new Vector3(0, 0, 100), (float)Math.PI / 2, 100, 1000);
+
 
         public Form1()
         {
@@ -22,11 +26,13 @@ namespace Renderer3D
             Graphics g = e.Graphics;
 
             pyramid.Draw(g, camera);
+            sphere.Draw(g, camera);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            pyramid.modelMatrix *= Matrix4x4.CreateRotationZ((float)(10 * Math.PI / 180));
+            sphere.modelMatrix *= Matrix4x4.CreateRotationZ((float)(10 * Math.PI / 180));
+
             pictureBox1.Invalidate();
         }
 
@@ -34,5 +40,6 @@ namespace Renderer3D
         {
             timer1.Start();
         }
+
     }
 }
---
+++ Renderer3D/Sphere.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renderer3D
+{
+    internal class Sphere : Object3D
+    {
+
+        public Sphere(float r, int n1, int n2)
+        {
+            //wierzchołki:
+            for (int i = 0; i < n1; i++)
+                for (int j = 0; j < n2; j++)
+                {
+                    float fi = -MathF.PI / 2 + MathF.PI * i / (n1-1);
+                    float al = 2 * MathF.PI * j / n2;
+
+                    float x = r * MathF.Cos(fi) * MathF.Cos(al);
+                    float y = r * MathF.Cos(fi) * MathF.Sin(al);
+                    float z = r * MathF.Sin(fi);
+
+                    mesh.Vertices.Add(new Vector3(x,y,z));
+                }
+
+
+            //trójkaty/ściany:
+            for (int i = 1; i < n1*n2-n2; i++)
+            {
+                mesh.Triangles.Add((i, i - 1, i + n2 - 1));
+                mesh.Triangles.Add((i, i + n2 - 1, i + n2));
+            }
+
+            //mesh.Triangles.Add((0, 1, 2));
+            //......
+
+        }
+
+    }
+}
